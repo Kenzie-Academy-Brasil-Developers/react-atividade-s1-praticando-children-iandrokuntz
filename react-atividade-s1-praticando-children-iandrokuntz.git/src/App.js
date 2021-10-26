3eb09b011a20c334ac949c54abfe3ed4e3295ebe
iandrokuntz <ianbertoluchi99@gmail.com>
2021-10-26 16:42:23 -0300

    atividade pronta

---
+++ react-atividade-s1-praticando-children-iandrokuntz.git/src/App.js
@@ -0,0 +1,18 @@
+import CenteredCard from './components/CenteredCard';
+import './App.css';
+
+function App() {
+
+  return (
+
+    <div className="App">
+
+      <CenteredCard> <h1>Children 1</h1> </CenteredCard>
+      <CenteredCard> <h1>Children 2</h1> </CenteredCard>
+      <CenteredCard> <h1>Children 3</h1> </CenteredCard>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ react-atividade-s1-praticando-children-iandrokuntz.git/src/components/CenteredCard/index.js
@@ -0,0 +1,14 @@
+import "./index.css"
+
+const CenteredCard = ({children}) => {
+
+
+    return(
+
+        <div>
+            <span> {children} </span>
+        </div>
+    )
+}
+
+export default CenteredCard;
